22bc0a17d29b0a80183a056a039a258c22dddee0
sabinechang <sabinechang@liontech.com.tw>
2018-06-22 16:13:50 +0800

    modify

--- module.js
+++ module.js
@@ -78,7 +78,7 @@ __webpack_require__(2);
 /* 1 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\n@import \"../core/core/mixins\";\r\n^\r\n      File to import not found or unreadable: ../core/core/mixins.\r\n      in E:\\Data\\Desktop\\Sabine\\module-calendar\\css.scss (line 4, column 1)\n    at runLoaders (E:\\Data\\Desktop\\Sabine\\module-calendar\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at E:\\Data\\Desktop\\Sabine\\module-calendar\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at E:\\Data\\Desktop\\Sabine\\module-calendar\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (E:\\Data\\Desktop\\Sabine\\module-calendar\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (E:\\Data\\Desktop\\Sabine\\module-calendar\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (E:\\Data\\Desktop\\Sabine\\module-calendar\\node_modules\\neo-async\\async.js:7974:18)\n    at options.error (E:\\Data\\Desktop\\Sabine\\module-calendar\\node_modules\\node-sass\\lib\\index.js:294:32)");
 
 /***/ }),
 /* 2 */
